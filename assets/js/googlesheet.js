a51c92b12fc76d54da0066b0332e81fd13a03aa2
Felipe Luiz Wlodkowski <wlodfelipe@gmail.com>
2023-10-11 09:40:55 -0300

    Alert do cadastro

--- assets/js/googlesheet.js
+++ assets/js/googlesheet.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(response => alert("Seu cadastro foi realizado! Obrigado por apoiar nosso jogo ;)" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
