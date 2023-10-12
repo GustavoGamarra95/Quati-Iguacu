73ac7c56dd8c2202604a333e4e96e16ab7d58e37
Felipe Luiz Wlodkowski <wlodfelipe@gmail.com>
2023-10-12 14:45:23 -0300

    Retirar cadastro funcional v1 pra cima deles

--- assets/js/googlesheet.js
+++ assets/js/googlesheet.js
@@ -1,10 +1,15 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw5wTNVOIRsWpr9v8hI98GN5kTRsn7KfWNkgc7FXEtGx7CnTLzl-MUo3SZky7fgmT_k/exec'
 
 const form = document.forms['contact-form']
+const formName = form.getAttribute('name');
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+
+  const formData = new FormData(form);
+  formData.append('formName', formName);
+
+  fetch(scriptURL, { method: 'POST', body: formData })
   .then(response => alert("Seu cadastro foi realizado! Obrigado por apoiar nosso jogo ;)" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
