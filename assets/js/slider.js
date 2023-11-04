c0b1d0cba83c55797ae85d3f0a78d73d0e8d7685
João Pedro <joao.pcanhete@gmail.com>
2023-11-03 23:28:20 -0300

    tradução introduzida, transição para bootstrap

--- assets/js/slider.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){ //so funciona se o site estiver carregado
-    $('.sliderimage ul').slick({
-        autoplay: true, 
-        autoplaySpeed: 1000, // tempo em milisegundos
-        dots: true, // pontinhos
-    });
-});
