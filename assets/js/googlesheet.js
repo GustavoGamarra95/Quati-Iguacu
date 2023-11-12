fcec9d3e6636667326f1816e7f93a457a56620c2
Gustavo <gugaro8@gmail.com>
2023-11-11 22:24:53 -0500

    atualizado

--- assets/js/googlesheet.js
+++ assets/js/googlesheet.js
@@ -1,16 +1,29 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw5wTNVOIRsWpr9v8hI98GN5kTRsn7KfWNkgc7FXEtGx7CnTLzl-MUo3SZky7fgmT_k/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw5wTNVOIRsWpr9v8hI98GN5kTRsn7KfWNkgc7FXEtGx7CnTLzl-MUo3SZky7fgmT_k/exec';
 
-const form = document.forms['contact-form']
-const formName = form.getAttribute('name');
+function validateForm() {
+    var name = document.getElementById('name').value.trim();
+    var email = document.getElementById('email').value.trim();
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+    if (name === '') {
+        alert('É necessário inserir um nome.');
+        return false;
+    }
 
-  const formData = new FormData(form);
-  formData.append('formName', formName);
+    if (email === '') {
+        alert('É necessário inserir um email.');
+        return false;
+    }
 
-  fetch(scriptURL, { method: 'POST', body: formData })
-  .then(response => alert("Seu cadastro foi realizado! Obrigado por apoiar nosso jogo ;)" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+    const form = document.forms['contact-form'];
+    const formName = form.getAttribute('name');
+
+    const formData = new FormData(form);
+    formData.append('formName', formName);
+
+    fetch(scriptURL, { method: 'POST', body: formData })
+    .then(response => alert("Seu cadastro foi realizado! Obrigado por apoiar nosso jogo ;)" ))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message));
+
+    return false; // Retorne false para evitar a submissão real do formulário
+}
